b95bea2032ce373e09ae8e063fda5c95329d418d
yanun0323 <a1256212562@gmail.com>
2022-06-23 01:39:27 +0800

    fix test suite name

--- suite_test/suite_test.go
+++ suite_test/suite_test.go
@@ -12,24 +12,23 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type TestSuite struct {
+type SVCSuite struct {
 	suite.Suite
 	repo domain.IRepository
 }
 
-func (su *TestSuite) SetupSuite() {
+func (su *SVCSuite) SetupSuite() {
 	fmt.Println("SetupSuite")
 	config.Init(".", "config")
 	db := repository.ConnectDB()
 	su.repo = repository.NewRepo(db)
 }
 
-func TestTestSuite(t *testing.T) {
-	suite.Run(t, new(TestSuite))
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(SVCSuite))
 }
 
-func (su *TestSuite) TestCreate() {
-	fmt.Println("TestCreate")
+func (su *SVCSuite) TestCreate() {
 	su.repo.AutoMigrate(&model.TestStruct{})
 	obj := model.TestStruct{Date: time.Date(2022, 3, 21, 0, 0, 0, 0, time.Local)}
 	obj.State = true
